c1552165e2d5c2d8ea15de6fd7708f4230ed1c2e
kistars <sunllen2014@outlook.com>
2025-08-14 17:59:49 +0800

    feat:modify config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
-const { PRIVATE_KEY, ALCHEMY_API_KEY, ETHERSCAN_KEY } = process.env;
+const { PRIVATE_KEY_ONE, PRIVATE_KEY_TWO, ALCHEMY_API_KEY, ETHERSCAN_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -17,7 +17,7 @@ module.exports = {
   networks: {
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [PRIVATE_KEY]
+      accounts: [PRIVATE_KEY_ONE, PRIVATE_KEY_TWO]
     }
   },
   etherscan: {
